4a13691cfe16ab70d139b1e9eec2a5b47238d4aa
BURAK GÖKSU <gogsub58365@gmail.com>
2021-03-13 23:55:29 +0300

    18.ders sonu

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+
+
+  products:Product[] = [];
+  dataLoaded = false;
+ 
+
+  constructor(private productService:ProductService , private activedRoute:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    
+    this.activedRoute.params.subscribe(params =>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }
+      else{
+        this.getProducts();
+      }
+    })
+
+  }
+
+
+  getProducts(){
+      this.productService.getProducts().subscribe(response => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      })
+    }
+
+    getProductsByCategory(categoryId:number){
+      this.productService.getProductsByCategory(categoryId).subscribe(response => {
+        this.products = response.data;
+        this.dataLoaded = true;
+      })
+    }
+
+  }
+
+
+
